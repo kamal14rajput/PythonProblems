dfaf38a14f85065f234f47c206a768e217b3a50b
kamal14rajput <kamal14rajput@gmail.com>
2022-02-09 19:27:28 +0530

    ADDED: nth harmonic number

---
+++ BasicCorePrograms/HarmonicNumber.py
@@ -0,0 +1,11 @@
+N = int(input("Enter the Nth harmonic number")) #nth harmonic number
+
+harmonic_number = 0
+if N > 0:
+    i = 1
+    while i <= N:
+        harmonic_number = harmonic_number + (1/i)
+        i += 1
+    print("The ", N,"th harmonic value is: ", harmonic_number)
+else:
+    print("Please enter number greater than 0 only!!!")
